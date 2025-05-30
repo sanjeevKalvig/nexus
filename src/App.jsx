3542e3a1d0f57def52e9775add2de4d97a434b85
sanjeevKalvig <sanjeev.kumar@kalvig.in>
2025-05-30 14:58:49 +0530

    herosectiomfixed2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import AboutUs from "./components/home/AboutUs";
 import ContactUs from "./components/home/ContactUS";
 import Footer from "./components/home/Footer";
-import HeroSection from "./components/home/HeroSection";
+import HeroSection from "./components/home/heroSection";
 import OurPeople from "./components/home/Ourpeople";
 import OurServices from "./components/home/OurServices";
 
 export default function App() {
   return (
     <>
-      <HeroSection />
+      <HeroSection/>
       <div className="flex h-2 justify-between opacity-40">
         <div className="bg-gray-300 opacity-4 w-[30%]" ></div>
         <div className="bg-gray-300 w-[30%] "></div>
